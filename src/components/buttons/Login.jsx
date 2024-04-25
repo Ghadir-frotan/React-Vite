e7a75951768fdc0371a0947ffbf6b11c9eddb0b8
Ghadir-frotan <159515152+Ghadir-frotan@users.noreply.github.com>
2024-04-25 09:25:33 +0430

    new changes

--- src/components/buttons/Login.jsx
+++ src/components/buttons/Login.jsx
@@ -1,41 +1,49 @@
-import {useState} from 'react';
+import  { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
 
 const Login = () => {
-const [username,setUsername] = useState('');
-const [password,setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    console.log('username',username);
-    console.log('password',password);
-};
-return (
-<div>
-  <h2>Login Form</h2>
-    <form onSubmit={handleSubmit}>
-        <div className="row col-md-6 mx-auto">
 
+    if (username === 'Ghadir' && password === '12345') {
+      setError('');
+      
+      alert('Logged in successfully!');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleLogin}>
         <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" 
-           className='form-control'
-          id="username" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} /><br></br>
-          </div>
-          <div> 
-            <label htmlFor="password">Password:</label>
-            <input type="password"
-            className='form-control'
-            id='password'
+          <label >Username:</label>
+          <input className='form-control'
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input className='form-control'
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            /><br></br>
+          />
         </div>
-        <button className='btn btn-info' type="submit">Login</button>
-        </div>
-        </form>
+        <Button type="submit">Login</Button>
+      </form>
     </div>
-);
+  );
 };
+
 export default Login;
